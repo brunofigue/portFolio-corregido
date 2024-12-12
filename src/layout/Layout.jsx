0254617193d9162326c302a08b3c2b2ca351750f
bruno <brunofigueroa2290@gmail.com>
2024-12-12 11:42:33 -0300

    cambio

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useTheme } from '../components/context/ThemeContext'; // Ajusta la ruta si es necesario
+import { useTheme } from '../components/context/ThemeContext'; 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
